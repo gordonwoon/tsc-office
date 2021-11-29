f4d307f930afb28580e3c8e2fbc455bf671995d9
Gordon Woon <gordonwoon@live.com>
2021-11-29 22:39:13 +0800

    use full path

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import { Server } from 'colyseus'
 import { monitor } from '@colyseus/monitor'
+const path = require('path')
 
 // import socialRoutes from "@colyseus/social/express"
 
@@ -13,7 +14,14 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('../client/build'))
+
+const buildPath = path.join(__dirname, "/../client/build")
+app.use(express.static(buildPath))
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/../client/build/index.html'));
+});
 
 const server = http.createServer(app)
 const gameServer = new Server({
